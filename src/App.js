1024f4a7ae48e6d24e488ea0caa935df66ebc214
kurlayusha <tbark.guard@gmail.com>
2022-11-05 20:39:25 +0300

    Form errors added(not finished) - added onChanges to forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+
 import RightPage from './components/RightPage'
 import frontCard from './images/bg-card-front.png'
 import backCard from './images/bg-card-back.png'
+import Form from './components/Form'
+import Thanks from './components/Thanks'
 // import backGround from './images/bg-main-mobile.png'
 
 function App() {
+  const [cardNum, setCardNum] = useState('0010 0000 0000 0000')
+  const [name, setName] = useState('Yusha Kuralay')
+  const [expDateM, setExpDateM] = useState('01')
+  const [expDateY, setExpDateY] = useState('06')
+  const [cvv, setCvv] = useState('001')
+
   return (
     <div className="App">
       <div className="container">
@@ -11,18 +21,29 @@ function App() {
         <div className="front--card">
           <img src={frontCard} />
           <div className="front--card--info">
-            <h1>0000 0000 0000 0000</h1>
-            <h2>Your Name</h2>
+            <h1>{cardNum}</h1>
+            <h3>{name}</h3>
+            <h3>
+              {expDateM}/{expDateY}
+            </h3>
           </div>
         </div>
         <div className="back--card">
           <img src={backCard} />
           <div className="front--card--info">
-            <h3>000</h3>
+            <h3>{cvv}</h3>
           </div>
         </div>
       </div>
-      <RightPage />
+      <Form
+        setCardNum={setCardNum}
+        setName={setName}
+        setExpDateM={setExpDateM}
+        setExpDateY={setExpDateY}
+        setCvv={setCvv}
+      />
+
+      <Thanks />
     </div>
   )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,132 @@
-function Form() {
+import { useState } from 'react'
+
+function Form(props) {
+  const [error, setError] = useState()
+  const [validError, setValidError] = useState()
+
+  const normalizeCardNumber = (value) => {
+    return (
+      value
+        .replace(/\s/g, '')
+        .match(/.{1,4}/g)
+        ?.join(' ')
+        .substr(0, 19) || ''
+    )
+  }
+
+  function handleNameChange(e) {
+    props.setName(e.target.value)
+  }
+
+  function handleCardNumChange(e) {
+    props.setCardNum(e.target.value)
+  }
+
+  function handleExpDateMChange(e) {
+    props.setExpDateM(e.target.value)
+  }
+
+  function handleExpDateYChange(e) {
+    props.setExpDateY(e.target.value)
+  }
+
+  function handleCvvChange(e) {
+    props.setCvv(e.target.value)
+  }
+
+  function handleBlankError(e) {
+    const target = e.target
+
+    if (isNaN(target.value)) {
+      setValidError('Please enter a valid name')
+    } else {
+      setValidError('')
+    }
+
+    if (!target.value) {
+      setError(`cannot be blank`)
+    } else {
+      setError('')
+    }
+  }
+
+  function handleValidError(e) {
+    const target = e.target
+    console.log(!isNaN(target.value))
+  }
+
+  // console.log(!isNaN('3'))
+
   return (
     <form>
       <label>CARDHOLDER NAME</label>
-      <input type="text" placeholder="e.g. Jane Appleseed" />
+      <input
+        name="CARDHOLDER NAME"
+        type="text"
+        placeholder="e.g. Jane Appleseed"
+        onChange={(e) => {
+          handleNameChange(e)
+          handleBlankError(e)
+          handleValidError(e)
+        }}
+      />
+      <br />
+      {error && <span>{error}</span>}
+      {validError && <span>{validError}</span>}
+      <br />
       <label>CARD NUMBER</label>
-      <input type="number" placeholder="e.g. 1234 5678 9123 0000" />
+      <input
+        placeholder="e.g. 1234 5678 9123 0000"
+        type="tel"
+        inputMode="numeric"
+        autoComplete="cc-number"
+        onChange={(e) => {
+          handleBlankError(e)
+          handleCardNumChange(e)
+          const { value } = e.target
+          e.target.value = normalizeCardNumber(value)
+        }}
+      />
+      <br />
+      {error && <span>{error}</span>}
+      <br />
       <label>EXP. DATE</label>
       <>
-        <input type="number" placeholder="MM" />
-        <input type="number" placeholder="YY" />
+        <input
+          type="text"
+          maxLength={2}
+          placeholder="MM"
+          onChange={(e) => {
+            handleBlankError(e)
+            handleExpDateMChange(e)
+          }}
+        />
+        <input
+          type="text"
+          maxLength={2}
+          placeholder="YY"
+          onChange={(e) => {
+            handleBlankError(e)
+            handleExpDateYChange(e)
+          }}
+        />
       </>
+      <br />
+      {error && <span>{error}</span>}
+      <br />
       <label>CVV</label>
-      <input type="number" placeholder="e.g. 123" />
+      <input
+        maxLength={4}
+        type="text"
+        placeholder="e.g. 123"
+        onChange={(e) => {
+          handleBlankError(e)
+          handleCvvChange(e)
+        }}
+      />
+      <br />
+      {error && <span>{error}</span>}
+      <br />
       <button>Confirm</button>
     </form>
   )
